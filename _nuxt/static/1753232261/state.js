cdb639db56494f916ea2463efe4308023a740754
bxrqa <bxrqa@users.noreply.github.com>
2025-07-23 00:57:42 +0000

    deploy: a973333ed00617addef785d6fd272a10dcdd4e08

---
+++ _nuxt/static/1753232261/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1753232261",layout:"default",error:a,serverRendered:true,routePath:b,config:{name:"Stephen Fray Young",dates:"February 16th 1949 - Jan 30th 2025",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
